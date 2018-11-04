b1c1477aaf387d6243326ccf939e2e0dc5fad403
Andrei Bacos <andreibacos@gmail.com>
2018-11-04 20:08:48 +0200

    add format-raw-devices functionality

--- lib/Modules/ComputeHooks/ComputeHooks.psm1
+++ lib/Modules/ComputeHooks/ComputeHooks.psm1
@@ -762,6 +762,15 @@ function Invoke-InstallHook {
         Write-JujuWarning "Failed to set power scheme."
     }
     Start-TimeResync
+    
+    $cfg = Get-JujuCharmConfig
+    if ($cfg['format-raw-devices']) {
+        Get-Disk | Where partitionstyle -eq 'raw' | `
+        Initialize-Disk -PartitionStyle GPT -PassThru | `
+        New-Partition -AssignDriveLetter -UseMaximumSize | `
+        Format-Volume -FileSystem NTFS -Confirm:$false
+    }
+    
     $prereqReboot = Install-Prerequisites
     if ($prereqReboot) {
         Invoke-JujuReboot -Now
